bc97e40dfd85ae5064a1c48a06fd36d49ba8ca5a
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-06-29 19:48:55 -0700

    updated npm react router & dom. added folders for components, pages, utils

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-  }
+}
+  
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-to-books");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server is now serving real good on PORT ${PORT}!`);
+});
