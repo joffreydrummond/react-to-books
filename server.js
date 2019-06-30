34624f1ba0d328f632d61711e3b30e4b0de6e9a3
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-06-29 19:33:44 -0700

    copied react into views folder

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+    mongoose = require("mongoose"),
+    app = express(),
+    PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
